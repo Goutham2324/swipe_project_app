9808247c2a9bfd907d21c2829764451e0d5dec5f
swejan kothamasu <swejan@deskfactors.com>
2024-05-12 14:19:13 +0530

    Initial Commit

---
+++ swipe_project_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swipe_project_app
+//
+//  Created by Swejan Kothamasu on 12/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
